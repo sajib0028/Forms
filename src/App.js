424474811150cfc2df84e9f2e46555872f8364f5
Sajib132 <sajibb813@gmail.com>
2021-04-25 00:40:30 +0530

    Updated with two input form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  
+  const [lastname, setLastName] = useState('');
+
+  const[lastNameNew, setLastNameNew] = useState();
+  const[myName, setMyName] =useState();
+  
+  const InputEvent = (event) =>{
+    setName(event.target.value);
+  }
+
+
+  const InputEventTwo = (event) =>{
+    setLastName(event.target.value);
+  }
+
+
+  const buttonClick = (event) =>{
+    event.preventDefault();
+    setMyName(name);
+    setLastNameNew(lastname);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <form onSubmit={buttonClick}>
+      <h1>Hello {myName} {lastNameNew} </h1>
+      <input type="text" placeholder="Enter you name" defaultValue=""
+      onChange={InputEvent} value={name}
+
+      />
+      <br />
+      <input type="text" placeholder="Enter you password" defaultValue=""
+      onChange={InputEventTwo} value={lastname}
+
+      />
+      <br />
+      <button type="submit">Submit Me 😄</button>
+      </form>
     </div>
+    </>
   );
 }
 
